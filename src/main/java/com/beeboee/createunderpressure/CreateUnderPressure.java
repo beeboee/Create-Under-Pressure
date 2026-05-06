710827eb96a36f6ba5d7bd9d5233bd787b4e9f30
beeboee <benjamingibsonw@gmail.com>
2026-05-06 06:12:46 -0700

    Fix debug event registration constructor

--- src/main/java/com/beeboee/createunderpressure/CreateUnderPressure.java
+++ src/main/java/com/beeboee/createunderpressure/CreateUnderPressure.java
@@ -4,9 +4,7 @@ import com.beeboee.createunderpressure.debug.DebugStickEvents;
 import com.mojang.logging.LogUtils;
 import org.slf4j.Logger;
 
-import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(CreateUnderPressure.MOD_ID)
@@ -14,10 +12,8 @@ public class CreateUnderPressure {
     public static final String MOD_ID = "create_under_pressure";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public CreateUnderPressure(FMLJavaModLoadingContext context) {
-        IEventBus modEventBus = context.getModEventBus();
+    public CreateUnderPressure() {
         NeoForge.EVENT_BUS.register(DebugStickEvents.class);
-
         LOGGER.info("Create: Under Pressure loaded");
     }
 }
