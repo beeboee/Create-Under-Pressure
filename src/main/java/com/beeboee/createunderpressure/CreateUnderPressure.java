92301e05f17a9876a47f87143c3eff368a592aa4
beeboee <benjamingibsonw@gmail.com>
2026-05-06 06:09:15 -0700

    Register debug stick events

--- src/main/java/com/beeboee/createunderpressure/CreateUnderPressure.java
+++ src/main/java/com/beeboee/createunderpressure/CreateUnderPressure.java
@@ -1,17 +1,23 @@
 package com.beeboee.createunderpressure;
 
+import com.beeboee.createunderpressure.debug.DebugStickEvents;
+import com.mojang.logging.LogUtils;
 import org.slf4j.Logger;
 
-import com.mojang.logging.LogUtils;
-
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(CreateUnderPressure.MOD_ID)
 public class CreateUnderPressure {
     public static final String MOD_ID = "create_under_pressure";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public CreateUnderPressure() {
+    public CreateUnderPressure(FMLJavaModLoadingContext context) {
+        IEventBus modEventBus = context.getModEventBus();
+        NeoForge.EVENT_BUS.register(DebugStickEvents.class);
+
         LOGGER.info("Create: Under Pressure loaded");
     }
 }
