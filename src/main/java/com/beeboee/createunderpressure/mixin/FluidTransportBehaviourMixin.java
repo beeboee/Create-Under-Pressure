9db8ce56b08a569312645a2251e8dadddfe80cf5
beeboee <benjamingibsonw@gmail.com>
2026-05-08 02:33:18 -0700

    Disable experimental Create world IO supplement

--- src/main/java/com/beeboee/createunderpressure/mixin/FluidTransportBehaviourMixin.java
+++ src/main/java/com/beeboee/createunderpressure/mixin/FluidTransportBehaviourMixin.java
@@ -1,6 +1,5 @@
 package com.beeboee.createunderpressure.mixin;
 
-import com.beeboee.createunderpressure.pressure.CreateWorldIOSupplementService;
 import com.beeboee.createunderpressure.pressure.NetworkHeadBridgeService;
 import com.beeboee.createunderpressure.pressure.NetworkPressurePlanner;
 import com.beeboee.createunderpressure.visual.WorldExchangeVisualLayer;
@@ -15,7 +14,6 @@ public abstract class FluidTransportBehaviourMixin {
     @Inject(method = "tick", at = @At("TAIL"), remap = false)
     private void createUnderPressure$tickPipePressure(CallbackInfo ci) {
         FluidTransportBehaviour pipe = (FluidTransportBehaviour) (Object) this;
-        CreateWorldIOSupplementService.tickPipe(pipe);
         NetworkPressurePlanner.tickPipe(pipe);
         NetworkHeadBridgeService.tickPipe(pipe);
         WorldExchangeVisualLayer.tickPipe(pipe);
