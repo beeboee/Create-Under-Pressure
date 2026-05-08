e760ba147a557b513eafdf945f752be61195f015
beeboee <benjamingibsonw@gmail.com>
2026-05-08 16:31:35 -0700

    Route world IO through hose pulley service

--- src/main/java/com/beeboee/createunderpressure/mixin/FluidTransportBehaviourMixin.java
+++ src/main/java/com/beeboee/createunderpressure/mixin/FluidTransportBehaviourMixin.java
@@ -1,8 +1,7 @@
 package com.beeboee.createunderpressure.mixin;
 
-import com.beeboee.createunderpressure.pressure.AggregateTankWorldOutputService;
+import com.beeboee.createunderpressure.pressure.HosePulleyWorldIOService;
 import com.beeboee.createunderpressure.pressure.NetworkHeadBridgeService;
-import com.beeboee.createunderpressure.pressure.NetworkPressurePlanner;
 import com.beeboee.createunderpressure.visual.WorldExchangeVisualLayer;
 import com.simibubi.create.content.fluids.FluidTransportBehaviour;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,8 +14,7 @@ public abstract class FluidTransportBehaviourMixin {
     @Inject(method = "tick", at = @At("TAIL"), remap = false)
     private void createUnderPressure$tickPipePressure(CallbackInfo ci) {
         FluidTransportBehaviour pipe = (FluidTransportBehaviour) (Object) this;
-        NetworkPressurePlanner.tickPipe(pipe);
-        AggregateTankWorldOutputService.tickPipe(pipe);
+        HosePulleyWorldIOService.tickPipe(pipe);
         NetworkHeadBridgeService.tickPipe(pipe);
         WorldExchangeVisualLayer.tickPipe(pipe);
     }
