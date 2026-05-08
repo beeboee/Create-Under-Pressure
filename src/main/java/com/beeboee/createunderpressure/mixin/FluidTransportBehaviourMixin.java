5933301b5b25d8b96313b82efc88553d798f854e
beeboee <benjamingibsonw@gmail.com>
2026-05-08 04:11:13 -0700

    Restore planner tick order

--- src/main/java/com/beeboee/createunderpressure/mixin/FluidTransportBehaviourMixin.java
+++ src/main/java/com/beeboee/createunderpressure/mixin/FluidTransportBehaviourMixin.java
@@ -1,7 +1,7 @@
 package com.beeboee.createunderpressure.mixin;
 
-import com.beeboee.createunderpressure.pressure.CreateWorldIOSupplementService;
 import com.beeboee.createunderpressure.pressure.NetworkHeadBridgeService;
+import com.beeboee.createunderpressure.pressure.NetworkPressurePlanner;
 import com.beeboee.createunderpressure.visual.WorldExchangeVisualLayer;
 import com.simibubi.create.content.fluids.FluidTransportBehaviour;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,7 +14,7 @@ public abstract class FluidTransportBehaviourMixin {
     @Inject(method = "tick", at = @At("TAIL"), remap = false)
     private void createUnderPressure$tickPipePressure(CallbackInfo ci) {
         FluidTransportBehaviour pipe = (FluidTransportBehaviour) (Object) this;
-        CreateWorldIOSupplementService.tickPipe(pipe);
+        NetworkPressurePlanner.tickPipe(pipe);
         NetworkHeadBridgeService.tickPipe(pipe);
         WorldExchangeVisualLayer.tickPipe(pipe);
     }
