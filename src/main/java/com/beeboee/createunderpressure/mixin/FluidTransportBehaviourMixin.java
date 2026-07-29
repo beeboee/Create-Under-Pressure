7f61d467c65b187b5d2f7a4d3da522fc931e2446
beeboee <benjamingibsonw@gmail.com>
2026-07-29 11:22:47 -0700

    Route all pipe work through the hydraulic runtime

--- src/main/java/com/beeboee/createunderpressure/mixin/FluidTransportBehaviourMixin.java
+++ src/main/java/com/beeboee/createunderpressure/mixin/FluidTransportBehaviourMixin.java
@@ -1,25 +1,16 @@
 package com.beeboee.createunderpressure.mixin;
 
-import com.beeboee.createunderpressure.pressure.HydraulicPlanService;
-import com.beeboee.createunderpressure.pressure.HydraulicPlanTankBridgeService;
-import com.beeboee.createunderpressure.pressure.HydraulicPlanWorldIOService;
-import com.beeboee.createunderpressure.pressure.HydraulicPlannerDebugService;
-import com.beeboee.createunderpressure.visual.WorldExchangeVisualLayer;
+import com.beeboee.createunderpressure.pressure.HydraulicRuntime;
 import com.simibubi.create.content.fluids.FluidTransportBehaviour;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(FluidTransportBehaviour.class)
+@Mixin(value = FluidTransportBehaviour.class, remap = false)
 public abstract class FluidTransportBehaviourMixin {
     @Inject(method = "tick", at = @At("TAIL"), remap = false)
-    private void createUnderPressure$tickPipePressure(CallbackInfo ci) {
-        FluidTransportBehaviour pipe = (FluidTransportBehaviour) (Object) this;
-        HydraulicPlanService.tickPipe(pipe);
-        HydraulicPlanWorldIOService.tickPipe(pipe);
-        HydraulicPlanTankBridgeService.tickPipe(pipe);
-        HydraulicPlannerDebugService.tickPipe(pipe);
-        WorldExchangeVisualLayer.tickPipe(pipe);
+    private void createUnderPressure$observePipe(CallbackInfo ci) {
+        HydraulicRuntime.observePipe((FluidTransportBehaviour) (Object) this);
     }
 }
